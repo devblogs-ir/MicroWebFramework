e17830d79ec3f7e687637b23dc0fff6ab415cbf8
Abolfazl Kabiri <Abolfazl_Kabiri@outlook.com>
2023-12-29 18:07:22 +0330

    handle exception

--- sources/Abolfazl Kabiri/MicroWebFramework.Presentation/Framework/ExceptionHandlingPipe.cs
+++ sources/Abolfazl Kabiri/MicroWebFramework.Presentation/Framework/ExceptionHandlingPipe.cs
@@ -1,6 +1,9 @@
 ﻿using Dumpify;
+using MicroWebFramework.Presentation.Common;
 using MicroWebFramework.Presentation.CustomExceptions;
+using MicroWebFramework.Presentation.Models;
 using MicroWebFramework.Presentation.Pipeline;
+using System.Net;
 
 namespace MicroWebFramework.Presentation.Framework;
 public class ExceptionHandlingPipe : Pipe
@@ -15,11 +18,24 @@ public class ExceptionHandlingPipe : Pipe
             "starting exception handling".Dump();
             Next(context);
         }
-        catch (InvalidIpAddressException ex) { "Invalid ip address ".Dump(ex.Message); }
-        catch (InaccessibilityException ex) { "Invalid request from ".Dump(ex.Message); }
-        catch (UnknownIpAddressException ex) { "Ip address is unknown ".Dump(ex.Message); }
-        catch (InvalidRequestException ex) { "Url is not valid ".Dump(ex.Message); }
-        catch (NotImplementedPipelineException) { "No pipes have been added to the pipeline".Dump(); }
-        catch (Exception ex) { "exception occurod".Dump(ex.Message); }
+        catch (InvalidIpAddressException ex) { "Invalid ip address ".Dump(ex.Message); HandleException(context, HttpStatusCode.Unauthorized); }
+        catch (InaccessibilityException ex) { "Invalid request from ".Dump(ex.Message); HandleException(context, HttpStatusCode.Unauthorized); }
+        catch (UnknownIpAddressException ex) { "Ip address is unknown ".Dump(ex.Message); HandleException(context, HttpStatusCode.Unauthorized); }
+        catch (InvalidRequestException ex) { "Url is not valid ".Dump(ex.Message); HandleException(context, HttpStatusCode.NotFound); }
+        catch (NotImplementedPipelineException) { "No pipes have been added to the pipeline".Dump(); HandleException(context, HttpStatusCode.BadGateway); }
+        catch (Exception ex) { "exception occurod".Dump(ex.Message); HandleException(context, HttpStatusCode.InternalServerError); }
+    }
+    private void HandleException(PipelineContext context, HttpStatusCode httpStatusCode)
+    {
+        context.HttpContext.Response.ContentType = "application/json";
+        context.HttpContext.Response.StatusCode = (int)httpStatusCode;
+        var buffer = context.HttpContext.Response.GetBytes(new Result<object>
+        {
+            IsSuccess = false,
+            StatusCode = (int)httpStatusCode,
+            Message = "an error has occurred"
+        });
+        context.HttpContext.Response.OutputStream.Write(buffer, 0, buffer.Length);
+        context.HttpContext.Response.Close();
     }
 }
