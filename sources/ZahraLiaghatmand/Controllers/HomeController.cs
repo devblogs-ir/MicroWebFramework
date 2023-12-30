d154a6e6de6b460665989922b78138f22b7a4caf
zahra liaghatmand <zahra.liaghatmand@gmail.com>
2023-12-30 21:14:29 +0330

    fix: exception Handling ToDo: think about how to handle more than one params

---
+++ sources/ZahraLiaghatmand/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PipelineDesignPattern.Controllers
+{
+    internal class HomeController
+    {
+        private readonly HttpContext _context;
+        public HomeController(HttpContext context)
+        {
+            _context = context;
+        }
+        public void Index()
+        {
+            Console.WriteLine("Welcome to Home");
+            _context.Response += "Welcome to Home";
+        }
+    }
+}
--- sources/ZahraLiaghatmand/Pipes/EndpointPipe.cs
+++ sources/ZahraLiaghatmand/Pipes/EndpointPipe.cs
@@ -18,23 +18,51 @@ namespace PipelineDesignPattern.Pipes
         {
             var parts = httpContext.Url.Split('/');
             int partsLength = parts.Length;
+            Controllers.NonController nonController = new(httpContext);
+            //Controller exception Handling
             var controllerClass = parts[1];
-            if (controllerClass == "favicon.ico") return;
-            string methodParam = null;
-            var actionMethod = parts[2];
+            if(controllerClass == string.Empty)
+            {
+                //Home
+                controllerClass = "Home";
+            }
+            var templateControllerName = $"PipelineDesignPattern.Controllers.{controllerClass}Controller";           
+            var typeController = Type.GetType(templateControllerName);
+            if(typeController == null)
+            {
+                nonController.Exception();
+                return;
+            }
+            //End of Controller exceptions
+
+            //Method exception Handling
+            string actionMethod = string.Empty;
+            if (partsLength > 2) {
+                actionMethod = parts[2];
+            }  
+            if(actionMethod == string.Empty)
+            {
+                actionMethod = "Index";
+            }
+            MethodInfo method = typeController.GetMethod(actionMethod);
+            if(method == null)
+            {
+                nonController.Exception();
+                return;
+            }
+            //End of Method exception Handling
+
+            string methodParam = string.Empty;
             if (partsLength > 3 && parts[3] != string.Empty) {
                 methodParam = parts[3];
             }
-            
-            var templateControllerName = $"PipelineDesignPattern.Controllers.{controllerClass}Controller";
-            var typeController = Type.GetType(templateControllerName);
-            MethodInfo method = typeController.GetMethod(actionMethod);
             var parametersInfo = method.GetParameters();
+
             var instance = Activator.CreateInstance(typeController, new[] { httpContext });
-            if (methodParam is not null)
+            if (methodParam != string.Empty)
             {
-                var userIdAsInt = Convert.ChangeType(methodParam, parametersInfo[0].ParameterType);
-                method.Invoke(instance, new[] { userIdAsInt });
+                var convertedParam = Convert.ChangeType(methodParam, parametersInfo[0].ParameterType);
+                method.Invoke(instance, new[] { convertedParam });
             }
             else method.Invoke(instance, null);
 
