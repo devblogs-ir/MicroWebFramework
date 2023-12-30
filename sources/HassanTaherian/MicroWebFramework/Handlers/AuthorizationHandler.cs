36d887ceefd4fe2d475c1da52bae6b91a4e5599b
Hassan Taherian <hassantsf.dev@gmail.com>
2023-12-30 17:27:55 +0330

    feat: Import source codes from PipelineDesignPattern project

---
+++ sources/HassanTaherian/MicroWebFramework/Handlers/AuthorizationHandler.cs
@@ -0,0 +1,23 @@
+﻿using Dumpify;
+
+namespace PipelineDesignPattern;
+public class AuthorizationHandler : BaseHandler
+{
+    private readonly IIpService _ipService;
+    public AuthorizationHandler(IIpService ipService)
+    {
+        _ipService = ipService;
+    }
+    public override void Handle(HttpContext context)
+    {
+        "Start Auth".Dump();
+
+        if (_ipService.IsOriginFromBannedCountries(context.IpAdrress))
+        {
+            var originCountry = _ipService.GetOriginCountry(context.IpAdrress);
+            throw new AccessingFromBannedCountryException(originCountry?.Name);
+        }
+
+        next?.Invoke(context);
+    }
+}
---
+++ sources/HassanTaherian/MicroWebFramework/PipelineBuilder.cs
@@ -0,0 +1,34 @@
+﻿namespace PipelineDesignPattern;
+public class PipelineBuilder
+{
+    private BaseHandler? entryPoint { get; set; }
+    private BaseHandler? lastHandler { get; set; }
+
+    public PipelineBuilder AddHandler(BaseHandler baseHandler)
+    {
+        if (IsThereHandlerInPipeline())
+        {
+            lastHandler.Next = baseHandler.Handle;
+        }
+        else
+        {
+            entryPoint = baseHandler;
+        }
+
+        lastHandler = baseHandler;
+
+        return this;
+    }
+
+    private bool IsThereHandlerInPipeline()
+    {
+        return lastHandler is not null;
+    }
+
+
+    public void Run(HttpContext httpContext)
+    {
+        entryPoint?.Handle(httpContext);
+    }
+
+}
