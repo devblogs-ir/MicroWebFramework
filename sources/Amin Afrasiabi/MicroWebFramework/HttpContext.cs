0e4740323a26e8d006d050ba08b167a667a76f0a
aminafra <mohamadamin.afrasiabi@gmail.com>
2024-01-01 00:04:44 +0330

    Add BaseController with Generic Ok method And Generic BaseResult

---
+++ sources/Amin Afrasiabi/MicroWebFramework/HttpContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+namespace PipelineDesignPattern;
+
+public class HttpContext
+{
+    public required string IP { get; set; }
+
+    public string Url { get; set; } = null!;
+
+    public HttpListenerRequest Request { get; set; } = null!;
+
+    public HttpListenerResponse Response { get; set; } = null!;
+
+    public CountryIPAddress Country => Enum.TryParse(IP[^2..], out CountryIPAddress country) ? country : 0;
+
+}
+public enum CountryIPAddress
+{
+    UnKnown = 0,
+    Iran = 10,
+    China = 20,
+    USA = 30,
+}
