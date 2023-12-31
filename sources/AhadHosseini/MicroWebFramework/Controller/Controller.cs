eca512ee47c2f11ae20cebe44f349eb6a39f7e88
AHHHM <AHHHM@AHMLB>
2023-12-31 13:00:15 +0330

    'fix:refactore'

--- sources/AhadHosseini/MicroWebFramework/Controller/Controller.cs
+++ sources/AhadHosseini/MicroWebFramework/Controller/Controller.cs
@@ -16,39 +16,8 @@ namespace MicroWebFramework.Controller
         {
             HttpContext = httpContext;
         }
-        public async void Ok2(object? value)
-        {
-            //HttpContext.Response.ContentType = "application/json";
-            //HttpContext.Response.StatusCode = 200;
-            //var buffer = HttpContext.Response.GetBytes(new Result<object>()
-            //{
-            //    IsSuccess = true,
-            //    StatusCode = (int)HttpStatusCode.OK,
-            //    Message = "Success",
-            //    Data = value
-            //});
-
-
-
-            var result = Encoding.UTF8.GetBytes(System.Text.Json.JsonSerializer.Serialize(value));
-            HttpContext.Response.OutputStream.Write(result, 0, result.Length);
-            HttpContext.Response.OutputStream.Close();
-        }
-
         public async void Ok(object? value)
         {
-            //HttpContext.Response.ContentType = "application/json";
-            //HttpContext.Response.StatusCode = 200;
-            //var buffer = HttpContext.Response.GetBytes(new Result<object>()
-            //{
-            //    IsSuccess = true,
-            //    StatusCode = (int)HttpStatusCode.OK,
-            //    Message = "Success",
-            //    Data = value
-            //});
-
-
-
             var result = Encoding.UTF8.GetBytes(System.Text.Json.JsonSerializer.Serialize(new OutPutModel<object>()
             {
                 IsSuccess = true,
@@ -59,10 +28,7 @@ namespace MicroWebFramework.Controller
             HttpContext.Response.OutputStream.Write(result, 0, result.Length);
             HttpContext.Response.OutputStream.Close();
         }
-
-
     }
-
    
     public class OutPutModel<TData>
     {
