5e246e950da67738aa5de577c5f1cb26cabcd9fd
aminafra <mohamadamin.afrasiabi@gmail.com>
2024-01-01 00:02:50 +0330

    Add Models And Controolers And Exceptions

---
+++ sources/Amin Afrasiabi/MicroWebFramework/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using MicroWebFramework.Models;
+
+namespace PipelineDesignPattern.Controllers;
+public class UsersController : BaseController
+{
+    public UsersController(HttpContext context) : base(context) { }
+
+    IList<Users> Users = new List<Users>()
+    {
+        new() { Id = 1, FullName = "Farid", },
+        new() { Id = 2, FullName = "Ali", },
+        new() { Id = 3, FullName ="Amir", },
+        new() { Id = 4, FullName = "Amin", },
+    };
+
+    public void GetAllUsers()
+    {
+        Ok(Users);
+    }
+
+    public void GetUserById(int id)
+    {
+        Ok(Users.FirstOrDefault(x => x.Id == id));
+    }
+}
---
+++ sources/Amin Afrasiabi/MicroWebFramework/Models/Users.cs
@@ -0,0 +1,6 @@
+﻿namespace MicroWebFramework.Models;
+public class Users
+{
+    public int Id { get; set; }
+    public string FullName { get; set; } = string.Empty;
+}
