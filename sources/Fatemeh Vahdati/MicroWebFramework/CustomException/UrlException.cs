9ba2368e5891d6aee6025720b56abed7ffefb7e1
vahdati847 <fatemeh.vahdati@gmail.com>
2023-12-31 19:32:24 +0330

    Add  MicroWebFramework Files

---
+++ sources/Fatemeh Vahdati/MicroWebFramework/CustomException/UrlException.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace MicroWebFramework.CustomException
+{
+    [Serializable]
+    internal class UrlException : Exception
+    {
+        public UrlException()
+        {
+        }
+
+        public UrlException(string? message) : base(message)
+        {
+        }
+
+        public UrlException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected UrlException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ sources/Fatemeh Vahdati/MicroWebFramework/PipelineBuilder.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MicroWebFramework.Pipes;
+using System.Net;
+
+public class PipelineBuilder
+{
+    private List<Type> _pipes = new List<Type>();
+
+    public PipelineBuilder AddPipe(Type pipe)
+    {
+        _pipes.Add(pipe);
+        return this;
+    }
+
+    public PipelineBuilder AddPipe<TType>()
+    {
+        AddPipe(typeof(TType));
+        return this;
+    }
+
+    public Action<HttpListenerContext> Build()
+    {
+        var selectedHandle = (Pipe)Activator.CreateInstance(_pipes[_pipes.Count - 1], null);
+        for (int i = _pipes.Count - 2; i > 0; i--)
+        {
+            selectedHandle = (Pipe)Activator.CreateInstance(_pipes[i], new[] { selectedHandle.Handle });
+        }
+
+        selectedHandle = (Pipe)Activator.CreateInstance(_pipes[0], selectedHandle.Handle);
+        return selectedHandle.Handle;
+    }
+}
