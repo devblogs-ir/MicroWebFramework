b27957ab892c94baadb8648f8f9f22fc9992042f
aminafra <mohamadamin.afrasiabi@gmail.com>
2024-01-01 00:03:19 +0330

    Complete Pipeline Pattern

---
+++ sources/Amin Afrasiabi/MicroWebFramework/Pipelines/AuthenticationPipe.cs
@@ -0,0 +1,23 @@
+﻿using Dumpify;
+using PipelineDesignPattern.Exceptions;
+
+namespace PipelineDesignPattern.Pipelines;
+
+public class AuthenticationPipe : Pipe
+{
+    public AuthenticationPipe() : base() { }
+    public AuthenticationPipe(Action<HttpContext> next) : base(next) { }
+
+    public override void Handle(HttpContext context)
+    {
+        "Start Authentication...".Dump();
+        ArgumentNullException.ThrowIfNull(context);
+
+        if (context.Country is CountryIPAddress.Iran)
+        {
+            throw new IranianIPBlockedException(message: "Iranian IPs are blocked");
+        }
+
+        if (_next is not null) _next(context);
+    }
+}
---
+++ sources/Amin Afrasiabi/MicroWebFramework/Pipelines/Builder/PipelineBuilder.cs
@@ -0,0 +1,48 @@
+﻿namespace PipelineDesignPattern.Pipelines.Builder;
+
+public class PipelineBuilder
+{
+    private readonly List<Type> _pipes = [];
+
+    public PipelineBuilder AddPipe(Type pipe)
+    {
+        if (!_pipes.Contains(pipe))
+        {
+            _pipes.Add(pipe);
+        }
+
+        return this;
+    }
+
+    public PipelineBuilder AddPipe<TType>() where TType : Pipe
+    {
+        AddPipe(typeof(TType));
+        return this;
+    }
+
+    public Action<HttpContext> Build()
+    {
+        var lastIndex = _pipes.Count - 1;
+
+        var selectedPipe = _pipes.ElementAtOrDefault(lastIndex);
+        ArgumentNullException.ThrowIfNull(selectedPipe);
+
+        var selectedHandler = Activator.CreateInstance(selectedPipe, null!) as Pipe;
+
+        if (_pipes.Count == 1)
+        {
+            return selectedHandler.Handle;
+        }
+
+        for (int i = lastIndex - 1; i > 0; i--)
+        {
+            selectedPipe = _pipes.ElementAtOrDefault(i);
+
+            selectedHandler = Activator.CreateInstance(selectedPipe!, new[] { selectedHandler.Handle }) as Pipe;
+        }
+
+        var firstHandler = Activator.CreateInstance(_pipes.First(), new[] { selectedHandler.Handle }) as Pipe;
+
+        return firstHandler.Handle;
+    }
+}
